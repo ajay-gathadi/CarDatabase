54d770c37320d0306bb3866ad977679171099459
Gathadi <ajay.gathadi@lseg.com>
2025-01-27 21:46:29 +0530

    Added CarController

--- src/main/java/org/gathadi/cardatabase/web/CarController.java
+++ src/main/java/org/gathadi/cardatabase/web/CarController.java
@@ -1,7 +1,20 @@
 package org.gathadi.cardatabase.web;
 
+import org.gathadi.cardatabase.domain.Car;
+import org.gathadi.cardatabase.domain.CarRepository;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CarController {
+    private final CarRepository carRepository;
+
+    public CarController(CarRepository carRepository) {
+        this.carRepository = carRepository;
+    }
+
+    @GetMapping("/cars")
+    public Iterable<Car> getCars() {
+        return carRepository.findAll();
+    }
 }
