408a06359f048e9da476b8d6b5a7d2c223680507
Ajay Gathadi <ajaygathadi@gmail.com>
2025-02-12 17:52:45 +0530

    Add OpenAPIConfiguration class

--- src/main/java/org/gathadi/cardatabase/OpenAPIConfiguration.java
+++ src/main/java/org/gathadi/cardatabase/OpenAPIConfiguration.java
@@ -1,18 +1,15 @@
 package org.gathadi.cardatabase;
 
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 
 @Configuration
 public class OpenAPIConfiguration {
     @Bean
     public OpenAPI carDatabaseOpenAPI() {
-        return new OpenAPI()
-                .info(new Info()
-                        .title("Car REST API")
-                        .description("Car Stock")
-                        .version("v1.0"));
+        return new OpenAPI().info(new Info().title("Car REST API").description("My car stock").version("1.0"));
     }
 }
