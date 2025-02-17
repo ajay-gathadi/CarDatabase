13a258a72fc08ee3a139f17183e39c86e328b5b8
Ajay Gathadi <ajaygathadi@gmail.com>
2025-02-17 16:16:29 +0530

    Delete userDetailsService method to disable in-memory users so that users can be fetched from the database

--- src/main/java/org/gathadi/cardatabase/SecurityConfiguration.java
+++ src/main/java/org/gathadi/cardatabase/SecurityConfiguration.java
@@ -12,11 +12,6 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
-    @Bean
-    public InMemoryUserDetailsManager userDetailsService(){
-        UserDetails user = User.builder().username("user").password(passwordEncoder().encode("password")).roles("USER").build();
-        return new InMemoryUserDetailsManager(user);
-    }
 
     @Bean
     public PasswordEncoder passwordEncoder(){
