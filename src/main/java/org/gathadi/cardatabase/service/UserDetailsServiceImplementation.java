1bbae3f6d3b1a509749e011dc5cb1be31d36fb5c
Ajay Gathadi <ajaygathadi@gmail.com>
2025-02-17 17:07:31 +0530

    Add UserDetailsServiceImplementation class to fetch the user from the database when Spring Security handles authentication

---
+++ src/main/java/org/gathadi/cardatabase/service/UserDetailsServiceImplementation.java
@@ -0,0 +1,34 @@
+package org.gathadi.cardatabase.service;
+
+import org.gathadi.cardatabase.domain.AppUser;
+import org.gathadi.cardatabase.domain.AppUserRepository;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.util.Optional;
+
+public class UserDetailsServiceImplementation implements UserDetailsService {
+    private final AppUserRepository appUserRepository;
+
+    public UserDetailsServiceImplementation(AppUserRepository appUserRepository) {
+        this.appUserRepository = appUserRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<AppUser> user = appUserRepository.findByUsername(username);
+        User.UserBuilder builder;
+
+        if(user.isPresent()){
+            AppUser currentUser = user.get();
+            builder = User.withUsername(username);
+            builder.password(currentUser.getPassword());
+            builder.roles(currentUser.getRole());
+        }else {
+            throw new UsernameNotFoundException("User not found");
+        }
+        return builder.build();
+    }
+}
