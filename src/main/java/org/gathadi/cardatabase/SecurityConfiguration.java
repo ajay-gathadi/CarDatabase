e9d5f43b2cb9a95797f3665deced811b38504cfe
Ajay Gathadi <ajaygathadi@gmail.com>
2025-02-17 16:26:59 +0530

    Add configureGlobal method to hash the password during the authentication process.

--- src/main/java/org/gathadi/cardatabase/SecurityConfiguration.java
+++ src/main/java/org/gathadi/cardatabase/SecurityConfiguration.java
@@ -1,15 +1,31 @@
 package org.gathadi.cardatabase;
 
+import org.gathadi.cardatabase.service.UserDetailsServiceImplementation;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
+    private final UserDetailsServiceImplementation userDetailsService;
+
+    public SecurityConfiguration(UserDetailsServiceImplementation userDetailsService) {
+        this.userDetailsService = userDetailsService;
+    }
+
+    /**
+     * This method defines the password hashing algorithm, BCryptPasswordEncoder,
+     * which encodes a hashed password during the authentication process.
+     * @param authenticationManagerBuilder
+     * @throws Exception
+     */
+    public void configureGlobal(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception{
+        authenticationManagerBuilder.userDetailsService(userDetailsService).passwordEncoder(new BCryptPasswordEncoder());
+    }
 
     @Bean
     public PasswordEncoder passwordEncoder(){
