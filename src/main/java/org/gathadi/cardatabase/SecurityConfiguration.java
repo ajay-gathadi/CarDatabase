a5829ad6224675f18d11fed0b664b26a9c34be0b
Ajay Gathadi <ajaygathadi@gmail.com>
2025-02-24 19:16:07 +0530

    Fix typo in configureGlobal method and update password encoder initialization

--- src/main/java/org/gathadi/cardatabase/SecurityConfiguration.java
+++ src/main/java/org/gathadi/cardatabase/SecurityConfiguration.java
@@ -4,6 +4,7 @@ import org.gathadi.cardatabase.service.UserDetailsServiceImplementation;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -20,12 +21,13 @@ public class SecurityConfiguration {
     /**
      * This method defines the password hashing algorithm, BCryptPasswordEncoder,
      * which encodes a hashed password during the authentication process.
-     * @param authenticationManagerBuilder
-     * @throws Exception
+     * @param authenticationManagerBuilder the builder used to create an `AuthenticationManager` with custom user details and password encoding
+     * @throws Exception if an error occurs while configuring the `AuthenticationManagerBuilder`
      */
-    public void configureGlobal(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception{
-        authenticationManagerBuilder.userDetailsService(userDetailsService).passwordEncoder(new BCryptPasswordEncoder());
+    public void configureGloabl(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception{
+        authenticationManagerBuilder.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
     }
+
 
     @Bean
     public PasswordEncoder passwordEncoder(){
