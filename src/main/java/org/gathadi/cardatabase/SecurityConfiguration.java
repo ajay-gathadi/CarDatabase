bb31631c7cc9e553fafb864beeab8e443ebbae72
Ajay Gathadi <ajaygathadi@gmail.com>
2025-03-03 21:58:40 +0530

    Add AuthenticationManager bean to SecurityConfiguration for improved authentication handling

--- src/main/java/org/gathadi/cardatabase/SecurityConfiguration.java
+++ src/main/java/org/gathadi/cardatabase/SecurityConfiguration.java
@@ -3,7 +3,9 @@ package org.gathadi.cardatabase;
 import org.gathadi.cardatabase.service.UserDetailsServiceImplementation;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -32,5 +34,10 @@ public class SecurityConfiguration {
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception{
+        return configuration.getAuthenticationManager();
     }
 }
