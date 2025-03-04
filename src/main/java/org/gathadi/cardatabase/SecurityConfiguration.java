0825153d29bc9913196205fa3b62a7d6e7e03e9c
Ajay Gathadi <ajaygathadi@gmail.com>
2025-03-04 12:28:01 +0530

    Add SecurityFilterChain bean to configure stateless session management and permit login requests through POST Method without authentication

--- src/main/java/org/gathadi/cardatabase/SecurityConfiguration.java
+++ src/main/java/org/gathadi/cardatabase/SecurityConfiguration.java
@@ -3,13 +3,17 @@ package org.gathadi.cardatabase;
 import org.gathadi.cardatabase.service.UserDetailsServiceImplementation;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
@@ -39,5 +43,15 @@ public class SecurityConfiguration {
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception{
         return configuration.getAuthenticationManager();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        http.csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement((session) -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests((authorizeHttpRequest) -> authorizeHttpRequest.requestMatchers(HttpMethod.POST,"/login").permitAll()
+                        .anyRequest().authenticated());
+
+        return http.build();
     }
 }
